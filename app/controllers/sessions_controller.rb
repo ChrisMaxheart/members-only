8e6a6fda5a0b4a755f8998de0945c5f031f0fe30
Samuel Henry Kurniawan <samuelhenrykurniawan@yahoo.com>
2018-12-29 07:00:34 +0800

    added authentication for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,13 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(:username => params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = "success!"
+    else
+      flash.now[:danger] = "failed!"
+    end
+    render :new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  resources :sessions
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
